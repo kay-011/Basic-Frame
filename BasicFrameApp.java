dc0a207341a0b452aec6f78c9d7c3a0eb0fbc704
kay-011 <139860862+kay-011@users.noreply.github.com>
2023-07-18 11:22:39 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author MIND BENDER
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MyFirstFrame myFrame;
+        
+        myFrame = new MyFirstFrame();
+
+    }
+    
+}
---
+++ MyFirstFrame.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author MIND BENDER
+ */
+public class MyFirstFrame extends JFrame {
+    
+    public MyFirstFrame(){
+        
+        setTitle("My First GUI");
+        setSize(200, 250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
+}
